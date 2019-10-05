ed9df5b01cf4e1a0e86abd2100e7fb1ce7c7ed0a
Nathan Duke <38300641+nbduke@users.noreply.github.com>
2019-10-05 16:36:20 -0700

    Initial React project setup (#1)
    - Configure basic React web app
    - Configure build tools, with linting

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+export default class App extends React.Component {
+  public render(): React.ReactElement {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            I want to play a game :)
+          </p>
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
